23118dbea0a12ba29b982b90204f2768ca5e9189
raemisch <sebastian.raemisch@cambrium.bio>
2024-10-03 16:51:53 +0200

    Slides added

--- ComplexInfo.py
+++ ComplexInfo.py
@@ -2,6 +2,7 @@
 
 from pyrosetta import *
 from pyrosetta.rosetta.protocols.analysis import InterfaceAnalyzerMover
+from pyrosetta.rosetta.core.scoring.sasa import SasaCalc
 
 
 class ComplexInfo():
@@ -18,6 +19,7 @@ class ComplexInfo():
         self.score = scorefxn(pose) 
         
         self.interface_analysis = None
+        self.if_hsasa = None
 
     def run_interface_analysis(self) -> None:
         # init('-detect_disulf false')
@@ -31,15 +33,25 @@ class ComplexInfo():
         interface_analyzer.apply(pose)
         self.interface_analysis = interface_analyzer
 
+        # Hydrophobic SASA
+        sasa_calc = SasaCalc()
+        sasa_calc.calculate(pose)
+        residue_sasa = list(sasa_calc.get_residue_hsasa())
+
+        if_residue_hsasa = [hsasa for i,hsasa in enumerate(residue_sasa) if i+1 in self.interface_analysis.get_interface_set()]
+        self.if_hsasa = sum(if_residue_hsasa)
+
+
     def get_interface_stats(self) -> dict:
         assert self.interface_analysis != None, "No interface analysis found"
 
         return {
             'dG':           round(self.interface_analysis.get_interface_dG(), 2),
-            'area':         round(self.interface_analysis.get_interface_delta_sasa(),2),
+            'SASA':         round(self.interface_analysis.get_interface_delta_sasa(),2),
+            'hyrophobic SASA': round(self.if_hsasa, 2),
             'num_res':      self.interface_analysis.get_num_interface_residues(),
             'unsats':       self.interface_analysis.get_interface_delta_hbond_unsat(),
-            'pymol_unsats': self.interface_analysis.get_pymol_sel_hbond_unsat(),
+            'packstat':     self.interface_analysis.get_interface_packstat(),
             'hbond_E':      round(self.interface_analysis.get_total_Hbond_E(),2),
             'residue_set':  self.interface_analysis.get_interface_set(),
         }
